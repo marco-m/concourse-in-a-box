0dd143ac8ff1118b791cf5296d7dc3d125478ef2
Marco Molteni <marco.molteni@pix4d.com>
2021-08-20 12:50:13 +0200

    fix: setup: add short sleep in minio-setup and vault-setup to avoid race condition
    Sometimes minio is not ready when minio-setup runs.
    This commit is a sort of an hack, we should investigate using `healthcheck`:
    - https://docs.docker.com/compose/compose-file/compose-file-v3/#healthcheck
    - https://docs.docker.com/engine/reference/builder/#healthcheck

--- scripts/minio-setup.sh
+++ scripts/minio-setup.sh
@@ -7,6 +7,13 @@ set -e
 : "${MINIO_ACCESS_KEY?Need to set this environment variable}"
 : "${MINIO_SECRET_KEY?Need to set this environment variable}"
 
+# FIXME This should be replaced by a more robust healthcheck, see
+# https://docs.docker.com/compose/compose-file/compose-file-v3/#healthcheck
+# https://docs.docker.com/engine/reference/builder/#healthcheck
+echo
+echo "***** Sleeping a few seconds to allow Minio to startup"
+sleep 5
+
 echo
 echo "***** Logging in to Minio"
 mc alias set concourse-minio "$MINIO_ADDR" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
